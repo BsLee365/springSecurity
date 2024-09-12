eaa30a7bbf8a4275f512c295b9cc08deec84c9c9
이범석 <44566130+BsLee365@users.noreply.github.com>
2024-09-12 22:51:57 +0900

    스프링 시큐리티 5까지 수강완료.

--- springSecurityEx/src/main/java/com/example/springSecurityEx/config/SecurityConfig.java
+++ springSecurityEx/src/main/java/com/example/springSecurityEx/config/SecurityConfig.java
@@ -5,11 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // 사용자 로그인시 비밀번호에 대해 단반향 해시 암호화를 진행하여 저장되어 있는 비밀번호와 대조한다.
+    // 스프링 시큐리티는 암호화를 위해 BCrypt Password Encoder를 제공하고 권장하고 있다.
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        // 암호화를 진행할 때 필요한 메서드
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -29,9 +38,7 @@ public class SecurityConfig {
                 .loginProcessingUrl("/loginProc")
                 .permitAll());
 
-        // 사용자 로그인시 비밀번호에 대해 단반향 해시 암호화를 진행하여 저장되어 있는 비밀번호와 대조한다.
-        // 스프링 시큐리티는 암호화를 위해 BCrypt Password Encoder를 제공하고 권장하고 있다.
-        
+
 
 
 
